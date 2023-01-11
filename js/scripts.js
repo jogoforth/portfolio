8b5dd71dc5a991ff8f7542909f860cb8d50bf798
jogoforth <121819524+jogoforth@users.noreply.github.com>
2023-01-11 16:57:28 -0500

    Added Initial JS for Kebab Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log ("Hello world")
+function menuToggle(){
+    var x = doctument.getElementById ('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
